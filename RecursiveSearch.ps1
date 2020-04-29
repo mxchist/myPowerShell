2534504c0fdc5c2c6b29818250f15e764864ce91
mchist <maxim.chistyakov@devim.team>
2020-04-29 22:24:40 +0300

    Recursive search: Simple and hard variants

--- RecursiveSearch.ps1
+++ RecursiveSearch.ps1
@@ -1,8 +1,29 @@
-﻿class ResultSearch {
+﻿ # Simple variant
+function Search-Text([String]$Path, [String]$IMatch) {
+     cd $Path;
+     $current_name = $( Get-Item . | Where-Object -Property Attributes -IMatch "Directory").Name;
+     if ($current_name -imatch  "visual" -or $current_name -eq "git" -or $current_name -eq "DWN-2858" -or $current_name -eq "Хрень с прошлой работы" ) {
+         cd ..;
+         return;
+     }
+         Get-Item -Path "*.sql"    | foreach {
+         Select-String -Pattern $IMatch -SimpleMatch -Path $_  |Select-Object -Property Path, Filename, Line;
+     }
+
+     $Paths = $(Get-ChildItem . | Where-Object -Property Attributes -IMatch "Directory");
+     foreach ($i in $Paths ) {
+         Search-Text -Path $i -IMatch $IMatch;
+      }
+      cd ..;
+  };
+
+
+  # Hard variant
+class ResultSearch {
 [Object]$File;
 [Int]$LineNumber;
 [String]$Line;
-}
+};
 
 function Search-Text([String]$Path, [String]$IMatch) {
     cd $Path;
@@ -14,19 +35,23 @@ function Search-Text([String]$Path, [String]$IMatch) {
     }
         Get-Item -Path "*.sql"    | foreach {
         $(Select-String -Pattern $IMatch -SimpleMatch -Path $_  |Select-Object -Property LineNumber, Line -OutVariable $a | Out-Null);
-        [ResultSearch]$temp = New-Object ResultSearch;
-        $temp.LineNumber = $a.LineNumber;
-        $temp.Line = $a.Line;
-        $temp.File = $_;
-        $ResultSet.add($temp);
+        if ($a -ne $null) {
+            [ResultSearch]$temp = New-Object ResultSearch;
+            $temp.LineNumber = $a.LineNumber;
+            $temp.Line = $a.Line;
+            $temp.File = $_;
+            $ResultSet.add($temp);
+
+            Remove-Variable a;
+            Remove-Variable temp;
+        }
     }
 
     $Paths = $(Get-ChildItem . | Where-Object -Property Attributes -IMatch "Directory");
     foreach ($i in $Paths ) {
-        Search-Text -Path $i -IMatch $IMatch;
+        $ResultSet.add($(Search-Text -Path $i -IMatch $IMatch));
      }
      cd ..;
      return $ResultSet;
  };
 
-  Search-Text -Path .\Documents\ -IMatch "planneddate" | Get-Member
